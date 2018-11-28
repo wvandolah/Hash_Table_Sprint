55d71ae8d67f1cca93f290504c344a0a68d759b4
William VanDolah <william.vandolah@gmail.com>
2018-11-28 17:21:08 -0600

    push

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -6,9 +6,28 @@
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
-
+  Answer *answer = malloc(sizeof(Answer));
+  // weights is an a pointer to array of weight
+  // storing weights as a key in object
+  // loop weights
+  // try to find a key in ht that equals limit - current 
+  // if found return answer if not added to ht
+  // answer should be index of weights not actual weight
+  for(int i = 0; i < length; i++){
+    int matcher = hash_table_retrieve(ht, limit - weights[i]);
+    printf("weights %d\n\n", weights[i]);
+    printf("matcher %d\n\n", matcher);
+    if(matcher == -1){
+      hash_table_insert(ht, weights[i], i);
+    }else{
+      answer->index_1 = i;
+      answer->index_2 = matcher;
+      destroy_hash_table(ht);
+      return answer;
+    }
+  }
   // YOUR CODE HERE
-
+  destroy_hash_table(ht);
   return NULL;
 }
 
--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -8,7 +8,26 @@ char **reconstruct_trip(Ticket **tickets, int length)
 {
   HashTable *hash = create_hash_table(16);
   char **route = malloc(length * sizeof(char *));
-
+  // find start ticket. should be the one with NULL set
+  // add start to route[0] add rest to ht with key as source and value as dest
+  for(int i = 0; i < length; i++){
+    printf("tickets %s\n\n", tickets[i]->source);
+    if(strcmp(tickets[i]->source, "NONE")== 0){
+      route[0] = tickets[i]->destination;
+    }else{
+      hash_table_insert(hash, tickets[i]->source, tickets[i]->destination);
+    }
+  }
+   for(int j = 1; j < length; j++){
+    //  retrieve returns NULL if it doesn't have a key
+     printf("whats in ht value when asking for route[0] %s\n\n\n", hash_table_retrieve(hash, route[0]));
+     if(hash_table_retrieve(hash, route[j - 1]) != NULL){
+       route[j] = hash_table_retrieve(hash, route[j - 1]);
+     }else{
+       route[j] = "NONE";
+     }
+   }
+  // 
   // YOUR CODE HERE
 
   return route;
